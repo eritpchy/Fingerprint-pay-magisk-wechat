84b3ff9372552d69ee0bb4e92a4b462fc2f106df
Jason Eric <eritpchy@qq.com>
2019-10-11 11:23:15 +0800

    * Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-./gradlew clean && ./gradlew :riru-module-xfingerprint-pay-wechat:zip && adb push ./release/* /sdcard/Download/
+#!/bin/bash
+set -e
+cd ${0%/*}
+./gradlew clean
+./gradlew :riru-module-xfingerprint-pay-wechat:zip
+adb push ./release/* /sdcard/Download/
