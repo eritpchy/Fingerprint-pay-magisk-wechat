a63c9b32b8fbca18025ced88833bfeb1bbbb0ae0
Jason Eric <eritpchy@qq.com>
2020-02-22 23:21:01 +0800

    + 3.9.7 fixed 切换为密码键盘时无法输入

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -e
 cd ${0%/*}
 ./gradlew clean
 ./gradlew :module:assembleRelease
-adb shell rm -fv "/data/local/tmp/libxfingerprint_pay_wechat.dex"
+adb shell rm -f "/data/local/tmp/libxfingerprint_pay_wechat.dex"
 adb push ./out/*.zip /sdcard/Download/
